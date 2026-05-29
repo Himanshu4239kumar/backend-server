2caf1b44bc8fc8b1f9bf98ee64363f4b06ce53ba
Himanshu Kumar <hmgamer20072003@gmail.com>
2026-05-29 21:44:21 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  url: "mongodb+srv://hk2509090_db_user:q37IMaND3svVCj3W@cluster0.smn47mw.mongodb.net/?appName=Cluster0",
+  url: "mongodb://hk2509090_db_user:q37IMaND3svVCj3W@ac-clmtpja-shard-00-00.smn47mw.mongodb.net:27017,ac-clmtpja-shard-00-01.smn47mw.mongodb.net:27017,ac-clmtpja-shard-00-02.smn47mw.mongodb.net:27017/?ssl=true&replicaSet=atlas-gvs8bc-shard-0&authSource=admin&appName=Cluster0",
 };
 //   mongodb+srv://hk2509090_db_user:dHEA0m1HpE62nhsj@cluster0.smn47mw.mongodb.net/?appName=Cluster0
 //    q37IMaND3svVCj3W
