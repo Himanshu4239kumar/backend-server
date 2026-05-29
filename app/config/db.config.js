d1f61ba6f27cdbbea4bbe644dfe0dc711daf634d
Himanshu Kumar <hmgamer20072003@gmail.com>
2026-05-29 20:25:13 +0530

    passwaord

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  url: "mongodb://localhost:27017/jsr",
+  // url: "mongodb://localhost:27017/jsr",
+  url: "mongodb+srv://hk2509090_db_user:dHEA0m1HpE62nhsj@cluster0.smn47mw.mongodb.net/?appName=Cluster0",
 };
+//   mongodb+srv://hk2509090_db_user:dHEA0m1HpE62nhsj@cluster0.smn47mw.mongodb.net/?appName=Cluster0
+//    dHEA0m1HpE62nhsj
