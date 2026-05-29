3e2a3eaa974dedc86199790bead0346a73777081
Himanshu Kumar <hmgamer20072003@gmail.com>
2026-05-29 21:39:24 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,7 @@
 module.exports = {
-  // url: "mongodb://localhost:27017/jsr",
-  url: "mongodb+srv://hk2509090_db_user:0QKg3Kktf3YCRcrf@cluster0.smn47mw.mongodb.net/?appName=Cluster0"
+  url: "mongodb+srv://hk2509090_db_user:q37IMaND3svVCj3W@cluster0.smn47mw.mongodb.net/?appName=Cluster0",
 };
 //   mongodb+srv://hk2509090_db_user:dHEA0m1HpE62nhsj@cluster0.smn47mw.mongodb.net/?appName=Cluster0
-//    dHEA0m1HpE62nhsj
+//    q37IMaND3svVCj3W
 
 //  mongodb+srv://hk2509090_db_user:<db_password>@cluster0.smn47mw.mongodb.net/?appName=Cluster0
