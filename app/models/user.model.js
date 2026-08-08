4b351eeab01ea9bfdb808190177b4ccf9a527a90
Himanshu Kumar <hmgamer20072003@gmail.com>
2026-08-08 10:05:58 +0530

    Update user.model.js

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -1,21 +1,22 @@
 const mongoose = require("mongoose");
 
-const User = mongoose.model(
-  "User",
-  new mongoose.Schema({
-    username: String,
-    firstName: String,
-    lastName: String,
-    email: String,
-    password: String,
-    roles: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Role"
-      }
-    ],
-    active: Boolean
-  })
-);
-const User = mongoose.models.user || mongoose.model("user", schema); // 'schema' ya jo bhi variable name ho
+// 1. Pehle humne Schema ko ek alag variable (UserSchema) mein daal diya
+const UserSchema = new mongoose.Schema({
+  username: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  password: String,
+  roles: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Role"
+    }
+  ],
+  active: Boolean
+});
+
+// 2. Phir Vercel ke liye safe wala model export kar diya
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+
 module.exports = User;
