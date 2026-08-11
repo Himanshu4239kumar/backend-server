7b22a3db226c1d8c18e3090d6597786579e24483
Himanshu Kumar <hmgamer20072003@gmail.com>
2026-08-11 08:52:15 +0530

    Update role.model.js

--- app/models/role.model.js
+++ app/models/role.model.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
-// 1. Schema ko alag variable (RoleSchema) mein daala
+// 1. Schema ko bahar nikala aur ek variable (RoleSchema) mein rakha
 const RoleSchema = new mongoose.Schema({
   name: String
 });
 
-// 2. Vercel ke liye safe wala model export
+// 2. Sirf ek baar 'const Role' banaya Vercel safe tarike se
 const Role = mongoose.models.Role || mongoose.model("Role", RoleSchema);
 
 module.exports = Role;
