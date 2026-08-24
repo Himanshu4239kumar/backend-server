f9c184ae9a962ba3e27041b0141a09da988bd589
Himanshu Kumar <hmgamer20072003@gmail.com>
2026-08-24 21:44:21 +0530

    fix model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -20,3 +20,4 @@ const UserSchema = new mongoose.Schema({
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
 
 module.exports = User;
+
--- app/models/wishlist.model.js
+++ app/models/wishlist.model.js
@@ -1,26 +1,24 @@
 module.exports = mongoose => {
-  const Wishlist = mongoose.model(
-    "wishlist",
-    mongoose.Schema(
-      {
-        userId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User",
-          required: true
-        },
-        productId: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-          required: true
-        },
-        status: {
-          type: Boolean,
-          default: true
-        }
+  const wishlistSchema = mongoose.Schema(
+    {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
       },
-      { timestamps: true }
-    )
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true
+      },
+      status: {
+        type: Boolean,
+        default: true
+      }
+    },
+    { timestamps: true }
   );
 
+  const Wishlist = mongoose.models.wishlist || mongoose.model("wishlist", wishlistSchema);
   return Wishlist;
-};
+}
